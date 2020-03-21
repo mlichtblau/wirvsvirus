60e0a6946f800a00a77d182f5d2a1f90883172f2
FWirtz <florian.wirtz08@gmail.com>
2020-03-21 22:51:50 +0000

    Add initial Questionnaire page

--- src/app/src/app/questionnaire/questionnaire.page.ts
+++ src/app/src/app/questionnaire/questionnaire.page.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionnairePage implements OnInit {
 
+  slideOpts = {
+    speed: 400,
+    centeredSlides: true,
+  };
+
   constructor() { }
 
   ngOnInit() {
