934acbb80187edfa0c7cc4ac9a60b423bfd2f670
tom2022 <32955754+tom2022@users.noreply.github.com>
2020-03-22 20:45:18 +0100

    map shows testcenters of database

--- src/app/src/app/components/testcenter-map/testcenter-map.component.ts
+++ src/app/src/app/components/testcenter-map/testcenter-map.component.ts
@@ -1,7 +1,8 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core'
 import { Geolocation } from '@ionic-native/geolocation/ngx'
+import {Storage} from '@ionic/storage';
 
-declare var google
+declare var google;
 
 @Component({
   selector: 'app-testcenter-map',
@@ -9,183 +10,128 @@ declare var google
   styleUrls: ['./testcenter-map.component.scss'],
 })
 export class TestcenterMapComponent implements OnInit {
-  @ViewChild('mapElement', { static: true }) mapElement: ElementRef
+  @ViewChild('mapElement', {static: true}) mapElement: ElementRef;
 
-  testcenters: {
-    testcenter: {
-      id: string
-      name: string
-      street: string
-      zipCode: string
-      city: string
-      pos: {
-        lat: number
-        lng: number
-      }
-      directions: string
-      waitingTime: number
-      openingHours: {
-        day: number
-        opensAt: string
-        closesAt: string
-      }[]
-    }[]
-  } = {
-    testcenter: [
-      {
-        id: '1',
-        name: 'Uni Klinikum Göttingen',
-        street: 'Robert-Koch-Straße',
-        zipCode: '37075',
-        city: 'Göttingen',
-        pos: {
-          lat: 51.5502,
-          lng: 9.9426,
-        },
-        directions: 'Anreise Anweisungen',
-        waitingTime: 500,
-        openingHours: [
-          {
-            day: 0,
-            opensAt: '8:00',
-            closesAt: '17:00',
-          },
-        ],
-      },
-      {
-        id: '21273',
-        name: 'Ev. Krankenhaus Göttingen',
-        street: 'An der Lutter 24',
-        zipCode: '37075',
-        city: 'Göttingen',
-        pos: {
-          lat: 51.55569,
-          lng: 9.94312,
-        },
-        directions: 'Anreise Anweisungen',
-        waitingTime: 500,
-        openingHours: [
-          {
-            day: 0,
-            opensAt: '8:00',
-            closesAt: '17:00',
-          },
-          {
-            day: 1,
-            opensAt: '8:00',
-            closesAt: '17:00',
-          },
-          {
-            day: 2,
-            opensAt: '8:00',
-            closesAt: '17:00',
-          },
-          {
-            day: 3,
-            opensAt: '8:00',
-            closesAt: '17:00',
-          },
-          {
-            day: 4,
-            opensAt: '8:00',
-            closesAt: '17:00',
-          },
-        ],
-      },
-    ],
+  constructor(
+    private geolocation: Geolocation,
+    public storage: Storage,
+  ) {
   }
-
-  constructor(private geolocation: Geolocation) {}
 
   ngOnInit() {
-    console.log(this.testcenters)
+    this.storage.get('testcenters')
+      .then((tcs) => {
+        this.loadMap(tcs);
+      });
   }
 
-  ngAfterContentInit(): void {
+  loadMap(testcenters) {
     this.geolocation
       .getCurrentPosition()
       .then(resp => {
         const pos = {
-          //lat: resp.coords.latitude,
-          //lng: resp.coords.longitude
-          lat: 51.545483,
-          lng: 9.905548,
-        }
-        const map = new google.maps.Map(this.mapElement.nativeElement)
-        const infoWindow = new google.maps.InfoWindow()
-        map.setCenter(pos)
-        map.setZoom(12)
+          lat: resp.coords.latitude,
+          lng: resp.coords.longitude,
+        };
+        const map = new google.maps.Map(this.mapElement.nativeElement);
+        map.setCenter(pos);
+        map.setZoom(12);
 
         //info window for location of user
 
         const patientIcon = {
           url: 'assets/icon/patienticon.png',
           scaledSize: new google.maps.Size(50, 50),
-        }
+        };
 
         let marker = new google.maps.Marker({
           position: pos,
           map: map,
           title: 'Ihr Standort',
           icon: patientIcon,
-        })
+        });
 
         const patientLocationText =
-          '<div id="content">' + `<p>Ihr Standort</p>` + '</div>'
+          '<div id="content">' + `<p>Ihr Standort</p>` + '</div>';
         const patientInfoWindow = new google.maps.InfoWindow({
           content: patientLocationText,
           maxWidth: 400,
           position: pos,
-        })
+        });
 
-        marker.addListener('click', function() {
+        marker.addListener('click', function () {
           patientInfoWindow.open(map, marker)
-        })
+        });
 
         //icons + info windows for test centers
 
         const testcenterIcon = {
           url: 'assets/icon/tcicon.png',
           scaledSize: new google.maps.Size(50, 50),
-        }
+        };
 
-        this.testcenters.testcenter.forEach(tc => {
+        testcenters.forEach(tc => {
           let marker = new google.maps.Marker({
             position: tc.pos,
             map: map,
             title: tc.name,
             icon: testcenterIcon,
-          })
+          });
 
-          const contentString =
-            '<div class="testcenter" id="content">' +
-            '<div id="bodyContent">' +
-            `<p><b>${tc.name}</b> <br> ${tc.street} <br> ${tc.zipCode} ${tc.city}</p>` +
-            `<p>Heute: ${tc.openingHours[getCurrentDay()].opensAt}-${
-              tc.openingHours[getCurrentDay()].closesAt
-            } <br>` +
-            `Wartezeit: etwa ${tc.waitingTime} Minuten</p>` +
-            `<ion-button color="medium" expand="block" href="../patient/queue-confirmation/${tc.id}">Auswählen</ion-button>` +
-            '</div>' +
-            '</div>'
+          const tcLocationText = createTCLocationText(tc);
+
           const testCenterInfoWindow = new google.maps.InfoWindow({
-            content: contentString,
+            content: tcLocationText,
             maxWidth: 400,
             position: tc.pos,
-          })
+          });
 
-          marker.addListener('click', function() {
+          marker.addListener('click', function () {
             testCenterInfoWindow.open(map, marker)
           })
         })
       })
       .catch(error => {
         console.log('Error getting location', error)
-      })
+      });
 
     function getCurrentDay() {
-      var d = new Date()
+      var d = new Date();
       return d.getDay()
+    }
+
+    function createTCLocationText(tc) {
+
+      let tcLocationText = '<div class="testcenter" id="content">' +
+        '<div id="bodyContent">' +
+        '<p>';
+
+      if(tc.name){
+        tcLocationText = tcLocationText + `<b>${tc.name}</b> <br>`;
+      }
+      if(tc.street){
+        tcLocationText = tcLocationText + `${tc.street} <br>`;
+      }
+      if(tc.zipCode && tc.city){
+        tcLocationText = tcLocationText + `${tc.zipCode} ${tc.city} <br>`;
+      }
+      tcLocationText = tcLocationText + '</p>' + '<p>';
+      if(tc.openingHours){
+        tcLocationText = tcLocationText + `Heute: ${tc.openingHours[getCurrentDay()].opensAt}-${
+          tc.openingHours[getCurrentDay()].closesAt
+          } <br>`;
+      }
+      if(tc.waitingTime){
+        tcLocationText = tcLocationText + `Wartezeit: etwa ${tc.waitingTime} Minuten`;
+      }
+      tcLocationText = tcLocationText + '</p>';
+      if(tc.id){
+        tcLocationText = tcLocationText + `<ion-button color="medium" expand="block" href="../patient/queue-confirmation/${tc.id}">Auswählen</ion-button>`;
+      }
+      tcLocationText = tcLocationText + '</div></div>';
+
+      return tcLocationText;
+
     }
   }
 }
