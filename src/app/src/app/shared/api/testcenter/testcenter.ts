d2a17ebd4559a1259df02525d612a9c7dbac8e26
Marius Lichtblau <marius@lichtblau.io>
2020-03-22 07:44:22 -0700

    Connect Login to Backend

--- src/app/src/app/shared/api/testcenter/testcenter.ts
+++ src/app/src/app/shared/api/testcenter/testcenter.ts
@@ -1,8 +1,13 @@
 import {Injectable} from '@angular/core';
 import {RestService} from '../rest.service';
 import {Testcenter} from '../../models/testcenter';
+import {TestcenterStaff} from '../../models/testcenter-staff';
 
 @Injectable()
 export class TestcenterProvider extends RestService<Testcenter> {
     resource: string = '/testcenters';
+
+    login(testcenterId, pin_code) {
+        return this.http.post<TestcenterStaff>(`${this.url}/${testcenterId}/staff_login`, { pin_code }, {headers: TestcenterProvider.headers()});
+    }
 }
