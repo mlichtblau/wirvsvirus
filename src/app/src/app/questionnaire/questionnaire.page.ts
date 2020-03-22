007d78c10f7c2957d51f3db2cd910bd9c6cab8c7
Marius Lichtblau <marius@lichtblau.io>
2020-03-22 11:19:31 -0700

    Allow less swiping

--- src/app/src/app/questionnaire/questionnaire.page.ts
+++ src/app/src/app/questionnaire/questionnaire.page.ts
@@ -1,17 +1,18 @@
-import { ViewChild } from '@angular/core'
+import {AfterViewInit, ViewChild} from '@angular/core';
 import { Component, OnInit } from '@angular/core'
 import { IonSlides } from '@ionic/angular'
 import { Patient, CriterionNames } from '../shared/models/patient'
 import { PatientProvider } from '../shared/api/patient/patient'
 import { map, pick } from 'lodash'
 import { getCriterions } from './criterions'
+import { Storage } from '@ionic/storage'
 
 @Component({
   selector: 'app-questionnaire',
   templateUrl: './questionnaire.page.html',
   styleUrls: ['./questionnaire.page.scss'],
 })
-export class QuestionnairePage implements OnInit {
+export class QuestionnairePage implements OnInit, AfterViewInit {
   @ViewChild(IonSlides, { static: false }) slides: IonSlides
 
   patientModel: Patient = {}
@@ -23,46 +24,41 @@ export class QuestionnairePage implements OnInit {
 
   criterions = getCriterions()
 
+  constructor(public patientProvider: PatientProvider, public storage: Storage) {}
+
   sendResults() {
     this.patientModel.anamnestic_items = map(this.criterions, item =>
       pick(item, ['criterion', 'answer'])
-    )
-    console.log(this.patientModel)
-    this.patientProvider.create(this.patientModel).subscribe(patient => {})
+    );
+    console.log(this.patientModel);
+    this.patientProvider.create(this.patientModel)
+        .subscribe(patient => {
+          this.storage.set('patient', patient)
+              .then(() => {
+                this.next();
+              });
+        });
   }
-
-  constructor(public patientProvider: PatientProvider) {}
 
   ngOnInit() {}
 
-  unlockOnChange(prop: string) {
-    if (this.allowNext(prop)) {
-      this.unlock()
-    }
+  ngAfterViewInit() {
+    this.slides.lockSwipes(true);
   }
 
   allowNext(prop: string) {
     return !!this.patientModel[prop]
   }
 
-  lock() {
-    // this.slides.lockSwipes(true)
-  }
-
-  unlock() {
-    this.slides.lockSwipes(false)
-  }
-
   next() {
+    this.slides.lockSwipes(false);
     this.slides.slideNext()
+    this.slides.lockSwipes(true);
   }
 
   prev() {
+    this.slides.lockSwipes(false);
     this.slides.slidePrev()
-  }
-
-  nextAndLock() {
-    this.next()
-    // this.lock()
+    this.slides.lockSwipes(true);
   }
 }
