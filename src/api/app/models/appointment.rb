9690dbb0e6fee169922b4f41458f445ce432eb84
Julius Rueckin <julius.rueckin@student.hpi.de>
2020-03-22 01:57:45 +0100

    untested first functionality of automated next appointment date and waiting number calculation

--- src/api/app/models/appointment.rb
+++ src/api/app/models/appointment.rb
@@ -4,6 +4,14 @@ class Appointment < ApplicationRecord
   belongs_to :feedback, optional: true
   belongs_to :appointment, foreign_key: :rescheduled_to_appointment_id, optional: true
 
+  before_save :generate_automated_appointment_data
+
+  def generate_automated_appointment_data
+    self.waiting_number = generate_next_waiting_number
+    last_appointment_time = self.testcenter.appointments.order(:appointment_time).last
+    self.appointment_time = last_appointment_time + self.testcenter.test_slot_duration(last_appointment_time.wday)
+  end
+
   def self.generate_next_waiting_number
     todays_appointments = Appointment.where('appointment_time BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day)
     return (todays_appointments.order(:appointment_time).last.waiting_number.to_i + 1).to_s
--- src/api/app/models/testcenter.rb
+++ src/api/app/models/testcenter.rb
@@ -4,8 +4,10 @@ class Testcenter < ApplicationRecord
 
   has_many :criterions_testcenters
   has_many :criterions, through: :criterions_testcenters
+  has_many :appointments
   belongs_to :coordinate
   belongs_to :contact_datum
+  has_many :opening_hours
 
   
   accepts_nested_attributes_for :coordinate
@@ -13,6 +15,10 @@ class Testcenter < ApplicationRecord
 
   def self.all_verified
     return Testcenter.where.not(verified_at: nil).all
+  end
+
+  def test_slot_duration(week_day)
+    self.daily_capacity / (self.opening_hours.find_by(day: week_day).closes_at - self.opening_hours.find_by(day: week_day).opens_at)
   end
 
 end
